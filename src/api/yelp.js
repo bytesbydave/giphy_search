1a54787bfc362fb46c8e774f0f7ae372437bed7c
Dave <dave.lin21@gmail.com>
2018-12-03 23:50:53 -0500

    Rename files

--- src/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses/search',
-  header: {
-    Authorization: 'Bearer'+ process.env.REACT_APP_YELP_API_KEY
-  }
-});
