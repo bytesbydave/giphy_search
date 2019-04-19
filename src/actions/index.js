4176fbaf9f0a69a523f900a56126bee932b330c9
Dave <dave.lin21@gmail.com>
2019-04-18 22:58:37 -0400

    create redux actions for search and giphy request

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_GIPHY_PENDING,
+  REQUEST_GIPHY_SUCCESS,
+  REQUEST_GIPHY_FAILED
+} from './types';
+import giphy from '../api/giphy';
+
+export const setSearchField = text => ({
+  type: CHANGE_SEARCH_FIELD,
+  payload: text
+});
+
+export const requestGiphys = term => async dispatch => {
+  dispatch({ type: REQUEST_GIPHY_PENDING });
+  try {
+    const response = await giphy.get('/v1/gifs/search', {
+      params: {
+        q: term
+      }
+    });
+    dispatch({ type: REQUEST_GIPHY_SUCCESS, payload: response.data });
+  } catch (err) {
+    dispatch({ type: REQUEST_GIPHY_FAILED, payload: err });
+  }
+};
