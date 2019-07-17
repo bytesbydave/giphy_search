8db2c9e53e0c7432d04da20cc50fa5a386d61cdb
Dave <dave.lin21@gmail.com>
2019-07-16 22:13:24 -0400

    Updates

---
+++ precache-manifest.708cd38bbcf1b459df2499b9177bbf1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67f8086eb3cddc52ceaa",
+    "url": "/giphy_search/static/css/main.d1902ed5.chunk.css"
+  },
+  {
+    "revision": "67f8086eb3cddc52ceaa",
+    "url": "/giphy_search/static/js/main.327c45bd.chunk.js"
+  },
+  {
+    "revision": "1729c6902ecf07981c1e",
+    "url": "/giphy_search/static/js/runtime~main.bca749eb.js"
+  },
+  {
+    "revision": "a09e9accf5c8aefd13d3",
+    "url": "/giphy_search/static/js/2.188d6d65.chunk.js"
+  },
+  {
+    "revision": "32371fe6ce8ef314cab29b76984c7593",
+    "url": "/giphy_search/index.html"
+  }
+];
